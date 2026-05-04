c9406a9a3fc91ed89563396707f37da206dd38ac
{中浦佐和｝ <{sawasawa003838@gmail.com}>
2026-05-04 21:03:54 +0900

    食品名と賞味期限をPOST送信し、Flask側でrequest.formから受け取る処理を追加

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        name = request.form["name"]
+        date = request.form["date"]
+
+        print(name,date)
+        
     return render_template("index.html")
 
 if __name__ == "__main__":
