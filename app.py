a183c515991f8b3debe34109e926ae0ea78765db
{中浦佐和｝ <{sawasawa003838@gmail.com}>
2026-05-05 11:53:34 +0900

    食品名と賞味期限をSQLiteに保存する機能を実装

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS foods (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            date TEXT NOT NULL
+        )      
+    """)
+
+    conn.commit()
+    conn.close()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         name = request.form["name"]
         date = request.form["date"]
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO foods (name, date) VALUES (?, ?)",
+            (name, date)
+        )
+
+        conn.commit()
+        conn.close()
 
         print(name,date)
-        
+
     return render_template("index.html")
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
     
